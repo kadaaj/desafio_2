70ef1689cc21f9307eafea35ba56386472852085
Gabriel <gabrielkadaaj@yahoo.com.br>
2025-09-20 05:11:03 -0300

    sql fix

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,7 +13,7 @@ INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (1, 1)
 INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (2, 1);
 INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (3, 1);
 
-INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (2, 2);
+INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (1, 2);
 INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (3, 2);
 INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (4, 2)
 
