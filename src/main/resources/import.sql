8c4f6319281ddd262a9370ab7a30a093984fbbd7
Gabriel <gabrielkadaaj@yahoo.com.br>
2025-09-20 04:57:10 -0300

    sql new

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,6 +10,14 @@ INSERT INTO tb_atividade (id, nome, descricao, preco) VALUES (1, 'Curso de HTML'
 INSERT INTO tb_atividade (id, nome, descricao, preco) VALUES (2, 'Oficina de Github', 'Controle versões de seus projetos', 50.00);
 INSERT INTO tb_atividade (id, nome, descricao, preco) VALUES (3, 'Palestra de IA', 'Introdução à IA', 100.00);
 
+INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (1, 1);
+INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (2, 1);
+INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (3, 1);
+
+INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (2, 2);
+INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (3, 2);
+INSERT INTO atividade_participante (participante_id, atividade_id) VALUES (4, 2)
+
 INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES ('2017-09-25 08:00:00', '2017-09-25 11:00:00', 1);
 INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES ('2017-09-25 14:00:00', '2017-09-25 18:00:00', 2);
 INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES ('2017-09-26 08:00:00', '2017-09-26 11:00:00', 3);
